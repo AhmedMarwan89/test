68a173aa6b64f1e95fbff837712a4a441df0d5e6
AhmedMarwan89 <ahmed37dn3k5@gmail.com>
2021-09-14 15:15:44 +0300

    search

---
+++ search.js
@@ -0,0 +1,14 @@
+// the tree is seperated to bigger and smaller , so it's not
+//read about binary search
+// the linear search should
+function search(x){
+arr = [ 1 ,2 ,3 ,4 , 5 , 6, 7, ,8 ]
+for ( i of arr){
+    if (x==i){
+        return ( arr.indexOf(i))
+            }    
+}return 'notfound'}
+
+console.log(search(3)) // there is function called filter for arraies. and find and find index 
+// usee the same way of zyad when he find the ring in ryada .this is also called binary research 
+//  الكي تشيله بذاكرة و هو يغنيك عن تسجيل الدخول . في حال بعت اللابتوب تروح على الحساب و تقول ريموف هذا الجهاز 
